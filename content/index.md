44a86227ead9393502bec1d25672e004f909a914
hua <2122295973@qq.com>
2024-10-20 12:17:33 +0800

    Quartz sync: Oct 20, 2024, 12:17 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Welcome to 南行's blog
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+欢迎来到我的博客，我是来自广西的南行。
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "南行",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
